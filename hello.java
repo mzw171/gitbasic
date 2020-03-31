6b7f9e344e15f078f1cc17b97b440a2b655451ea
mzw171 <42522351+mzw171@users.noreply.github.com>
2020-03-31 14:37:09 -0400

    added an interesting function

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
 	
+public void doSomethingInteresting() {
+	//doing intersting stuffs here
+}
+	
  public static void main(String[] args){
   System.out.println("Hello Git");
  }

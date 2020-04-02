a85d0fa590f80de7a9b85144d9fdd1e31d1e658e
mzw171 <42522351+mzw171@users.noreply.github.com>
2020-04-02 14:11:51 -0400

    fixing issue 53

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@ public class hello{
 	
 public void doSomethingInteresting() {
 	//doing intersting stuffs here
+	int a = 10;
+	int b = 20; 
+	int c = a + b;
 }
 	
  public static void main(String[] args){
